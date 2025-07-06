3c441ef76effb6088a36ba45c336435c336d52dc
Lori Civello <lacivello@gmail.com>
2025-07-06 15:24:26 -0400

    Fix Clerk middleware usage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,7 @@
-import { auth } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-export default auth(() => {
-  // This is where you could add rules to protect pages,
-  // but for now, we leave it empty to allow access to all pages.
-});
+export default authMiddleware();
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!api|_next|.*\\..*).*)"], // adjust if needed
 };
