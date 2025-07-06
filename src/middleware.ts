b2f5e3494a93c8766bf94d23d75b4407d3934e66
Lori Civello <lacivello@gmail.com>
2025-07-06 15:07:12 -0400

    Use authMiddleware for route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  // Make the homepage public so everyone can see it.
-  // All other routes, like your dashboard, will be protected by default.
-  publicRoutes: ["/"] 
+export default authMiddleware({
+  // The homepage ("/") is accessible to everyone, logged in or not.
+  // All other pages, like "/dashboard", will be protected.
+  publicRoutes: ["/"],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
